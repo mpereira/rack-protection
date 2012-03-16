5e35bb1f29ae2c47e546b3befe86dbe24b2d25b6
Murilo Pereira <murilo@murilopereira.com>
2012-03-16 09:02:23 -0300

    URI doesn't parse nils.

--- lib/rack/protection/json_csrf.rb
+++ lib/rack/protection/json_csrf.rb
@@ -8,20 +8,24 @@ module Rack
     # More infos::         http://flask.pocoo.org/docs/security/#json-security
     #
     # JSON GET APIs are vulnerable to being embedded as JavaScript while the
-    # Array prototype has been patched to track data. Checks the referrer
+    # Array prototype has been patched to track data. Checks the HTTP origin
     # even on GET requests if the content type is JSON.
     class JsonCsrf < Base
       default_reaction :deny
 
       def call(env)
         status, headers, body = app.call(env)
-        allow_origin = headers['Access-Control-Allow-Origin']
-        if allow_origin != '*'
-          host = URI(allow_origin).host
-          if referrer(env) != Request.new(env).host && referrer(env) != host
-            result = react(env)
-            warn env, "attack prevented by #{self.class}"
+        http_access_control_allow_origin = headers['Access-Control-Allow-Origin']
+        http_origin = headers['Origin']
+        if http_access_control_allow_origin
+          if http_access_control_allow_origin == '*' ||
+               URI(allow_origin).host == http_origin
+            origin_allowed = true
           end
+        end
+        if http_origin != Request.new(env).host && !origin_allowed
+          result = react(env)
+          warn env, "attack prevented by #{self.class}"
         end
         result or [status, headers, body]
       end
